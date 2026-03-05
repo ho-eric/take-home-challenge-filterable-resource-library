499b0151c7d369c8e69ba4f7b4bbc6e269dde0e2
Eric <erho20@gmail.com>
2026-03-04 21:39:21 -0800

    makes entire card linked to the url

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,14 +35,15 @@ function renderResources(resources) {
 
     resources.forEach(item => {
         const clone = template.content.cloneNode(true);
+        const cardLinkWrapper = clone.querySelector('.card-link-wrapper');
 
+        cardLinkWrapper.href = item.url;
         clone.querySelector('.post-date').textContent = new Date(item.date).toLocaleDateString();
         clone.querySelector('.resource-title').textContent = item.title;
         clone.querySelector('.resource-excerpt').textContent = item.excerpt;
         clone.querySelector('.resource-content-type').textContent = item.contentType;
         clone.querySelector('.resource-condition').textContent = item.condition;
         clone.querySelector('.resource-author').textContent = item.author;
-        clone.querySelector('.read-more').href = item.url;
 
         grid.appendChild(clone);
     });
